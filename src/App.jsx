a1867cd3d3ab4e2a5d3bba504fd118de8058aeb5
Martin Orsag <martin.orsag@websupport.eu>
2024-05-22 12:04:34 +0200

    Added layout and routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import {
-  BrowserRouter,
-  Routes,
-  Route,
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
+import Layout from "./Layout";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<Home />
+        },
+        {
+          path:"/hotels",
+          element:<List/>
+        },
+        {
+          path:"/hotels/:id",
+          element:<Hotel/>
+        },
+      ]
+    }]);
+
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   );
 }
 
