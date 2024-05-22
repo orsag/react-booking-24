4903fc08d350e7823a2f90575ccc25ab0848c694
Martin Orsag <martin.orsag@websupport.eu>
2024-05-22 12:17:10 +0200

    Navbar comp.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,10 +5,10 @@ import { Outlet } from "react-router-dom";
 function Layout() {
   return (
     <div className="layout">
-      <div className="navbar">
+      <div className="layout-navbar">
         <Navbar />
       </div>
-      <div className="content">
+      <div className="layout-content">
         <Outlet/>
       </div>
     </div>
